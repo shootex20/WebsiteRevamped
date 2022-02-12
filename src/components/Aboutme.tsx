7b7039052840a59665fb0173d8680aff01561c1f
Chelsey <chelsey74@live.ca>
2022-02-12 13:05:39 -0700

    Added numerous changes

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, OutlinedInput, TextField } from "@mui/material";
 import React from "react";
 import "../styles/Aboutme.css";
 import Card from "@mui/material/Card";
@@ -20,51 +20,119 @@ function Aboutme() {
   });
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
+    <div
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        border: "2px solid #000000",
+        borderRadius: "25px",
+        width: "70%",
+      }}
     >
-      <Grid container sm={2}>
-        <Grid item component={Card}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              image={require("./Projects/images/profile.jpg")}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: "25px",
+          backgroundColor: "#121212",
+          padding: "5%",
+          width: "100%",
+          height: "100%",
+          backgroundImage:
+            "linear-gradient(rgba(255, 255, 255, 0.09), rgba(255, 255, 255, 0.09))",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} display={"flex"}>
+            <TextField
+              disabled
+              id="contained-disabled"
+              label="Education"
+              defaultValue="Graduated from Southern Alberta Institute of Technology majoring in Software Development."
+              multiline
+              fullWidth
+              rows={5}
             />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                About me
-              </Typography>
-              <br />
-              <Typography variant="body2" color="text.secondary">
-                Graduated from Southern Alberta Institute of Technology majoring
-                in Software Development.
-              </Typography>
-              <br />
-              <Typography variant="body2" color="text.secondary">
-                Programming Languages: Java, JavaScript/HTML/CSS, SQL, Bash, C#,
-                React.js, TypeScript.js
-              </Typography>
-              <br />
-              <Typography variant="body2" color="text.secondary">
-                Frameworks: Express.js, Node.js, .Net framework Tools: Azure,
-                Git, Azure DevOps, CI/CD, Maven, Tomcat
-              </Typography>
-              <br />
-              <Typography variant="body2" color="text.secondary">
-                I have experience in DevOps as well, mostly in Azure and setting
-                up new apps from scratch and tinkering with build pipelines on
-                deploying apps.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions></CardActions>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled
+              id="contained-disabled"
+              label="Experience"
+              defaultValue="Junior DevOps @ SparrowConnected.
+June 2021 - Current"
+              multiline
+              fullWidth
+              rows={5}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled
+              id="contained-disabled"
+              label="Programming languages"
+              defaultValue="Java, JavaScript/HTML/CSS, SQL, Bash, C#, React.js, TypeScript.js"
+              multiline
+              fullWidth
+              rows={5}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled
+              id="contained-disabled"
+              label="Tools and Frameworks"
+              defaultValue="Express.js, Node.js, .Net, Azure, Git, Azure DevOps, CI/CD, Maven, Tomcat"
+              multiline
+              fullWidth
+              rows={5}
+            />
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </div>
+    </div>
   );
 }
 
 export default Aboutme;
+
+/*     //   <Grid container sm={2}>
+    //     <Grid item component={Card}>
+    //       <CardActionArea>
+    //         <CardMedia
+    //           component="img"
+    //           image={require("./Projects/images/profile.jpg")}
+    //         />
+    //         <CardContent>
+    //           <Typography gutterBottom variant="h5" component="div">
+    //             About me
+    //           </Typography>
+    //           <br />
+    //           <Typography variant="body2" color="text.secondary">
+                Graduated from Southern Alberta Institute of Technology majoring
+                in Software Development. January 2020 - April 2002
+    //           </Typography>
+    //           <br />
+    //           <Typography variant="body2" color="text.secondary">
+                Programming Languages: Java, JavaScript/HTML/CSS, SQL, Bash, C#,
+                React.js, TypeScript.js
+    //           </Typography>
+    //           <br />
+    //           <Typography variant="body2" color="text.secondary">
+                Frameworks: Express.js, Node.js, .Net framework Tools: Azure,
+                Git, Azure DevOps, CI/CD, Maven, Tomcat
+    //           </Typography>
+    //           <br />
+    //           <Typography variant="body2" color="text.secondary">
+                // I have experience in DevOps as well, mostly in Azure and setting
+                // up new apps from scratch and tinkering with build pipelines on
+                // deploying apps.
+    //           </Typography>
+    //         </CardContent>
+    //       </CardActionArea>
+    //       <CardActions></CardActions>
+    //     </Grid>
+    //   </Grid> */
